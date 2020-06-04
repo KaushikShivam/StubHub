559a9b878ad00b8a841584799f5cbd5688928c41
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-06-04 16:32:41 +0530

    Scafolld signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,8 +2,8 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/api/users/currentUser', (req, res) => {
+router.post('/api/users/signin', (req, res) => {
 	res.send('hi there');
 });
 
-export { router as currentUserRouter };
+export { router as signinRouter };
