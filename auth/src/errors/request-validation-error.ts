68bec344c416aaee7c7e7209e7d294cbfba896f5
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-06-05 13:20:36 +0530

    Create requestvalidationerror

---
+++ auth/src/errors/request-validation-error.ts
@@ -0,0 +1,10 @@
+import { ValidationError } from 'express-validator';
+
+export class RequestValidationError extends Error {
+	constructor(private errors: ValidationError[]) {
+		super();
+
+		// Only beause we are extending a build is class
+		Object.setPrototypeOf(this, RequestValidationError.prototype);
+	}
+}
