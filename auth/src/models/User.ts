d7f216cf3384e3ba08d006c00542eaa3b4d3c43f
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-06-05 16:52:17 +0530

    Add typechecking issue for User model

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -7,8 +7,14 @@ interface UserAttrs {
 }
 
 // Interface that describes the properties that the User model has
-interface UserModel extends mongoose.Model<any> {
-	build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+	build(attrs: UserAttrs): UserDoc;
+}
+
+// Interface that describes the properties that the user model has
+interface UserDoc extends mongoose.Document {
+	email: string;
+	password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -24,6 +30,6 @@ const userSchema = new mongoose.Schema({
 
 userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
 
-const User = mongoose.model<any, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };
