ce5102b31fdc755d6b00a9078aa183d193ae27cb
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-06-05 13:24:18 +0530

    Add custom errors to signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from './../errors/request-validation-error';
+
+import { DatabaseConnectionError } from './../errors/database-connect-error';
 
 const router = express.Router();
 
@@ -16,12 +19,13 @@ router.post(
 		const errors = validationResult(req);
 
 		if (!errors.isEmpty()) {
-			throw new Error('Invalid email or password');
+			throw new RequestValidationError(errors.array());
 		}
 
 		const { email, password } = req.body;
 
 		// Create a user here
+		throw new DatabaseConnectionError();
 
 		res.send({});
 	}
