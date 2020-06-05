d9be80322ae9373fc818a7f15bb2397ffbc478b0
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-06-05 13:27:25 +0530

    Type check custom errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,11 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 
+import { RequestValidationError } from './../errors/request-validation-error';
+
+import { DatabaseConnectionError } from './../errors/database-connect-error';
+
 export const errorHandler = (
 	err: Error,
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) => {
+	if (err instanceof RequestValidationError) {
+	}
+
+	if (err instanceof DatabaseConnectionError) {
+	}
+
 	res.status(400).send({
 		message: err.message,
 	});
